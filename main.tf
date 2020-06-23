0c99b812e850ea4483145df0a4b2db00177e2e45
Colin Wilson <colin.wilson@protonmail.ch>
2020-06-23 13:08:03 +0100

    Add support for hcloud Load Balancer; Update kubernetes dependencies to v1.16.2 (HCCM - v1.6.0)

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "kubernetes_deployment" "hcloud_cloud_controller_manager" {
 
         host_network = true
         container {
-          image = "hetznercloud/hcloud-cloud-controller-manager:v1.5.2"
+          image = "hetznercloud/hcloud-cloud-controller-manager:v1.6.0"
           name  = "hcloud-cloud-controller-manager"
           command = [
             "/bin/hcloud-cloud-controller-manager",
